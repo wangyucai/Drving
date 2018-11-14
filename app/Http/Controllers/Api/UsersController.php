0c9c2e1d7eac8f5214310637be2f13ed7e6f8412
wangyucai <1228835707@qq.com>
2018-11-14 08:24:24 +0000

    小程序用户注册

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -6,10 +6,10 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Transformers\UserTransformer;
 use App\Http\Requests\Api\UserRequest;
-use App\Http\Requests\Api\WeappAuthorizationRequest;
 
 class UsersController extends Controller
 {
+    // 注册
     public function store(UserRequest $request)
     {
         $captchaData = \Cache::get($request->captcha_key);
@@ -52,58 +52,61 @@ class UsersController extends Controller
         return $this->response->item($this->user(), new UserTransformer());
     }
 
-    // 小程序登录
-    public function weappStore(WeappAuthorizationRequest $request)
+    // 小程序注册
+    public function weappStore(UserRequest $request)
     {
-        $code = $request->code;
+        $captchaData = \Cache::get($request->captcha_key);
 
-        // 根据 code 获取微信 openid 和 session_key
+        if (!$captchaData) {
+            return $this->response->error('图片验证码已失效', 422);
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        // 获取微信的 openid 和 session_key
         $miniProgram = \EasyWeChat::miniProgram();
-        $data = $miniProgram->auth->session($code);
+        $data = $miniProgram->auth->session($request->code);
 
-        // 如果结果错误，说明 code 已过期或不正确，返回 401 错误
         if (isset($data['errcode'])) {
             return $this->response->errorUnauthorized('code 不正确');
         }
 
-        // 找到 openid 对应的用户
+        // 如果 openid 对应的用户已存在，报错403
         $user = User::where('weapp_openid', $data['openid'])->first();
 
-        $attributes['weixin_session_key'] = $data['session_key'];
-
-        // 未找到对应用户则需要提交用户名密码进行用户绑定
-        if (!$user) {
-            // 如果未提交用户名密码，403 错误提示
-            if (!$request->username) {
-                return $this->response->errorForbidden('用户不存在');
-            }
-
-            $username = $request->username;
-
-            // 用户名可以是邮箱或电话
-            filter_var($username, FILTER_VALIDATE_EMAIL) ?
-                $credentials['email'] = $username :
-                $credentials['username'] = $username;
-
-            $credentials['password'] = $request->password;
-
-            // 验证用户名和密码是否正确
-            if (!Auth::guard('api')->once($credentials)) {
-                return $this->response->errorUnauthorized('用户名或密码错误');
-            }
-
-            // 获取对应的用户
-            $user = Auth::guard('api')->getUser();
-            $attributes['weapp_openid'] = $data['openid'];
+        if ($user) {
+            return $this->response->errorForbidden('微信已绑定其他用户，请直接登录');
         }
 
-        // 更新用户数据
-        $user->update($attributes);
+        // 创建用户
+        $user = User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'personal_name' => $request->personal_name,
+            'drive_school_name' => $request->drive_school_name,
+            'registration_site' => $request->registration_site,
+            'trainingground_site' => $request->trainingground_site,
+            'class_introduction' => $request->class_introduction,
+            'password' => bcrypt($request->password),
+            'weapp_openid' => $data['openid'],
+            'weixin_session_key' => $data['session_key'],
+        ]);
 
-        // 为对应用户创建 JWT
-        $token = Auth::guard('api')->fromUser($user);
+        // 清除图片验证码缓存
+        \Cache::forget($request->captcha_key);
 
-        return $this->respondWithToken($token)->setStatusCode(201);
+        // meta 中返回 Token 信息
+        return $this->response->item($user, new UserTransformer())
+            ->setMeta([
+                'access_token' => \Auth::guard('api')->fromUser($user),
+                'token_type' => 'Bearer',
+                'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+            ])
+            ->setStatusCode(201);
     }
 
 }
