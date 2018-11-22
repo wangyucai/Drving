7d7347a34c967332c781e10e2be821bdfc14878e
wangyucai <1228835707@qq.com>
2018-11-22 05:44:00 +0000

    ..

---
+++ app/Http/Requests/Api/FormRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Dingo\Api\Http\FormRequest as BaseFormRequest;
+
+
+class FormRequest extends BaseFormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+}
