804075eb1c0725f65897737d5f554e94867dfc55
wangyucai <1228835707@qq.com>
2018-12-04 14:21:53 +0000

    教练表设置优化

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -11,19 +11,6 @@ class UserTransformer extends TransformerAbstract
 {
     public function transform(User $user)
     {
-        if($user->all_time){
-            $all_time = explode(',', $user->all_time);
-            $all_time = Schedule::whereIn('id',$all_time)->pluck('time');
-            $a = [];
-            foreach ($all_time as $v) {
-                array_push($a, $v);
-            }
-            $user->all_time = implode(',',$a);
-        }
-        if($user->single_time){
-            $single_time = Schedule::where('id',$user->single_time)->first();
-            $user->single_time = $single_time->time;
-        }
         if($user->car_photo){
             $space_image_id = explode(',',$user->car_photo);
             $images = Image::whereIn('path',$space_image_id)->get();
@@ -49,14 +36,15 @@ class UserTransformer extends TransformerAbstract
             'trainingground_site' => $user->trainingground_site,
             'car_photo' => $user->car_photo,
             'car_photo_id' => $car_photo_id,
-            'all_time' => $user->all_time,
-            'single_time' => $user->single_time,
             'day_times' => $user->day_times,
             'introduction' => $user->introduction,
             'if_check' => $user->if_check,
             'car_photo' => $user->car_photo,
             'my_points' => $user->my_points,
             'pid' => $user->pid,
+            'one_level'=>$user->one_level,
+            'two_level'=>$user->two_level,
+            'three_level'=>$user->three_level,
         ];
     }
 }
